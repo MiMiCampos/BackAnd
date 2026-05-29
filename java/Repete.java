e10ec8f3c56786473bed5f2592d5089cf05ec406
Mirelle Campos <mirelle.campos.gois@gmail.com>
2026-05-28 19:48:38 -0500

    While

---
+++ java/Repete.java
@@ -0,0 +1,11 @@
+public class Repete {
+    public static void main(String[] args) throws Exception {
+        // While (enquanto)
+        // while (condição) {}
+        int contador = 0;
+        while (contador < 3) {
+            System.out.println("Olá");
+            contador++;
+        }
+    }
+}
